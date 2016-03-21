2c1b519793c20ed8f3c3d4429935a921800c7fb9
Derick Yang <derick.yang@tufts.edu>
2016-03-21 16:14:41 -0400

    forgot to change the database back

--- flask_rest_service/__init__.py
+++ flask_rest_service/__init__.py
@@ -5,13 +5,13 @@ from flask.ext.pymongo import PyMongo
 from flask import make_response
 from bson.json_util import dumps
 
-MONGO_URL = os.environ.get('MONGO_URL')
-if not MONGO_URL:
-    MONGO_URL = "mongodb://localhost:27017/rest";
+MONGOLAB_URI = os.environ.get('MONGOLAB_URI')
+if not MONGOLAB_URI:
+    MONGOLAB_URI = "mongodb://localhost:27017/rest";
 
 app = Flask(__name__)
 
-app.config['MONGO_URI'] = MONGO_URL
+app.config['MONGO_URI'] = MONGOLAB_URI
 mongo = PyMongo(app)
 
 def output_json(obj, code, headers=None):
